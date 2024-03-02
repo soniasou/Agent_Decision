3c22fa9b988c7da12276c3b09725f8e50b73592c
soniasou <48654943+soniasou@users.noreply.github.com>
2024-03-02 13:28:03 +0100

    Add files via upload

---
+++ agent.py
@@ -0,0 +1,85 @@
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=in_channels,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+        )
+        self.conv2 = nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=in_channels,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+        )
+
+    def forward(self, x):
+        out = nn.ReLU()(x)
+        out = self.conv1(out)
+        out = nn.ReLU()(out)
+        out = self.conv2(out)
+        return out + x
+
+
+class ImpalaBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(ImpalaBlock, self).__init__()
+        self.conv = nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+        )
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.res1 = ResidualBlock(out_channels)
+        self.res2 = ResidualBlock(out_channels)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)(x)
+        x = self.res1(x)
+        x = self.res2(x)
+        return x
+
+class ImpalaModel(nn.Module):
+    def __init__(self, in_channels=3, task_dim=32, **kwargs):
+        super(ImpalaModel, self).__init__()
+
+        self.task_dim = task_dim
+        self.block1 = ImpalaBlock(in_channels=in_channels, out_channels=16)
+        self.block2 = ImpalaBlock(in_channels=16, out_channels=32)
+        self.block3 = ImpalaBlock(in_channels=32, out_channels=self.task_dim)
+
+    def forward(self, x):
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = nn.ReLU()(x)
+        return x
+
+
+class Agent(nn.Module):
+    def __init__(self):
+        super(Agent, self).__init__()
+        self.features = ImpalaModel(in_channels=3)
+        self.policy = nn.Sequential(
+            # nn.ReLU(),
+            nn.Linear(in_features=32 * 8 * 8, out_features=256),
+            nn.ReLU(),
+            nn.Linear(256, 15)
+        )
+        
+    def forward(self, x):
+        z = self.features(x)
+        z = torch.flatten(z, 1)
+        return self.policy(z)
